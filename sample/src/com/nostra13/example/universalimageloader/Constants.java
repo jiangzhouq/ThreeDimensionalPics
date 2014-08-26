cc187303265f81386e2a9b855de513d8ca9f5412
jiangzhouq <jiangzhouq@gmail.com>
2014-08-26 11:18:36 +0800

    增加红蓝、左右选择界面

--- sample/src/com/nostra13/example/universalimageloader/Constants.java
+++ sample/src/com/nostra13/example/universalimageloader/Constants.java
@@ -19,7 +19,8 @@ package com.nostra13.example.universalimageloader;
  * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
  */
 public final class Constants {
-
+	
+	public static final boolean LOG_ENABLE = true;
 	public static final String[] IMAGES = new String[] {
 			// Heavy images
 			"https://lh6.googleusercontent.com/-55osAWw3x0Q/URquUtcFr5I/AAAAAAAAAbs/rWlj1RUKrYI/s1024/A%252520Photographer.jpg",
---
+++ sample/src/com/nostra13/example/universalimageloader/ModeChoiceActivity.java
@@ -0,0 +1,89 @@
+package com.nostra13.example.universalimageloader;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class ModeChoiceActivity extends Activity implements OnClickListener{
+	
+	private View mDecorView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		mDecorView = getWindow().getDecorView();
+		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+			setContentView(R.layout.mc_landscape);
+		}else{
+			setContentView(R.layout.mc_portrait);
+		}
+		hideSystemUI();
+		ImageView view1 = (ImageView) findViewById(R.id.honglan);
+		view1.setOnClickListener(this);
+		ImageView view2 = (ImageView) findViewById(R.id.zuoyou);
+		view2.setOnClickListener(this);
+	}
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.honglan:
+			break;
+		case R.id.zuoyou:
+			break;
+		}
+	}
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+			if (Constants.LOG_ENABLE) {
+				Log.d("qiqi", "横屏");
+			}
+			setContentView(R.layout.mc_landscape);
+		}else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+			if (Constants.LOG_ENABLE) {
+				Log.d("qiqi", "竖屏");
+			}
+			setContentView(R.layout.mc_portrait);
+		}
+	}
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+	        super.onWindowFocusChanged(hasFocus);
+	    if (hasFocus) {
+	    	mDecorView.setSystemUiVisibility(
+	                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+	}
+	// This snippet hides the system bars.
+	private void hideSystemUI() {
+	    // Set the IMMERSIVE flag.
+	    // Set the content to appear under the system bars so that the content
+	    // doesn't resize when the system bars hide and show.
+	    mDecorView.setSystemUiVisibility(
+	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+	            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+	            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+	}
+
+	// This snippet shows the system bars. It does this by removing all the flags
+	// except for the ones that make the content appear under the system bars.
+	private void showSystemUI() {
+	    mDecorView.setSystemUiVisibility(
+	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+	}
+	
+}
