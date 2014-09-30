8d0c7d3936138ff558fc64d18d908b46b3c3af44
jiangzhouq <jiangzhouq@gmail.com>
2014-09-30 11:36:44 +0800

    加载过程中增加进度提醒，增加无网络提示。

--- sample/src/com/jiangzhou/tdp/ModeChoiceActivity.java
+++ sample/src/com/jiangzhou/tdp/ModeChoiceActivity.java
@@ -2,11 +2,9 @@ package com.jiangzhou.tdp;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
@@ -22,13 +20,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.res.Configuration;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.net.UrlQuerySanitizer.ValueSanitizer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -36,19 +35,23 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
-public class ModeChoiceActivity extends Activity implements OnClickListener{
-	
+
+public class ModeChoiceActivity extends Activity implements OnClickListener {
+
 	private View mDecorView;
 	DisplayImageOptions options;
-	private Handler mHandler = new Handler(){
+	private Handler mHandler = new Handler() {
 		public void handleMessage(android.os.Message msg) {
-			switch(msg.what){
+			switch (msg.what) {
 			case 0:
+				TextView loading = (TextView) findViewById(R.id.warning);
+				loading.setVisibility(View.GONE);
 				LinearLayout menus = (LinearLayout) findViewById(R.id.menus);
 				menus.setVisibility(View.VISIBLE);
 				setListener();
@@ -56,114 +59,124 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 			}
 		};
 	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		mDecorView = getWindow().getDecorView();
-		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+		if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
 			setContentView(R.layout.mc_landscape);
-		}else{
+		} else {
 			setContentView(R.layout.mc_portrait);
 		}
 		options = new DisplayImageOptions.Builder()
-		.showImageForEmptyUri(R.drawable.ic_empty)
-		.showImageOnFail(R.drawable.ic_error)
-		.resetViewBeforeLoading(true)
-		.cacheOnDisk(true)
-		.imageScaleType(ImageScaleType.EXACTLY)
-		.bitmapConfig(Bitmap.Config.RGB_565)
-		.considerExifParams(true)
-		.displayer(new FadeInBitmapDisplayer(300))
-		.build();
-//		hideSystemUI();
+				.showImageForEmptyUri(R.drawable.ic_empty)
+				.showImageOnFail(R.drawable.ic_error)
+				.resetViewBeforeLoading(true).cacheOnDisk(true)
+				.imageScaleType(ImageScaleType.EXACTLY)
+				.bitmapConfig(Bitmap.Config.RGB_565).considerExifParams(true)
+				.displayer(new FadeInBitmapDisplayer(300)).build();
+		// hideSystemUI();
 		ImageView bg = (ImageView) findViewById(R.id.bg);
 		ImageLoader imageLoader = ImageLoader.getInstance();
-		imageLoader.displayImage("http://vrfessor.com/mybackground.jpg",bg, options);
-		
-//		Log.d("qiqi", "TDPProvider start onCreate");
-//		Cursor mCursor = getContentResolver().query(Page.CONTENT_URI, null, null, null, null);
-//		ContentValues value = new ContentValues();
-//		value.put(Page.COLUMN_DEFAULT_NAME, "nimabi");
-//		value.put(Page.COLUMN_DEFAULT_URL, "zhangwanglitishidai");
-//		value.put(Page.COLUMN_DEFAULT_CTIME, "20140901");
-//		getContentResolver().insert(Page.CONTENT_URI, value);
-		
-		startGetInfo();
-//		LinearLayout menus = (LinearLayout) findViewById(R.id.menus);
-//		menus.setVisibility(View.VISIBLE);
-//		setListener();
+		imageLoader.displayImage("http://vrfessor.com/mybackground.jpg", bg,
+				options);
+
+		// Log.d("qiqi", "TDPProvider start onCreate");
+		// Cursor mCursor = getContentResolver().query(Page.CONTENT_URI, null,
+		// null, null, null);
+		// ContentValues value = new ContentValues();
+		// value.put(Page.COLUMN_DEFAULT_NAME, "nimabi");
+		// value.put(Page.COLUMN_DEFAULT_URL, "zhangwanglitishidai");
+		// value.put(Page.COLUMN_DEFAULT_CTIME, "20140901");
+		// getContentResolver().insert(Page.CONTENT_URI, value);
+		if(isConnected()){
+			startGetInfo();
+		}else{
+			TextView loading = (TextView) findViewById(R.id.warning);
+			loading.setText(R.string.no_connect);
+		}
+		// LinearLayout menus = (LinearLayout) findViewById(R.id.menus);
+		// menus.setVisibility(View.VISIBLE);
+		// setListener();
 	}
-	private void setListener(){
+
+	private void setListener() {
 		Button view1 = (Button) findViewById(R.id.honglan);
 		view1.setOnClickListener(this);
 		Button view2 = (Button) findViewById(R.id.zuoyou);
 		view2.setOnClickListener(this);
 	}
+
 	@Override
 	public void onClick(View v) {
-		Intent intent = new Intent(this,CategoryActivity.class);
-		switch(v.getId()){
+		Intent intent = new Intent(this, CategoryActivity.class);
+		switch (v.getId()) {
 		case R.id.honglan:
-			intent.putExtra(Constants.MODE_CHOICE_NAME, Constants.MODE_CHOLICE_HONGLAN);
+			intent.putExtra(Constants.MODE_CHOICE_NAME,
+					Constants.MODE_CHOLICE_HONGLAN);
 			startActivity(intent);
 			break;
 		case R.id.zuoyou:
-			intent.putExtra(Constants.MODE_CHOICE_NAME, Constants.MODE_CHOLICE_ZUOYOU);
+			intent.putExtra(Constants.MODE_CHOICE_NAME,
+					Constants.MODE_CHOLICE_ZUOYOU);
 			startActivity(intent);
 			break;
 		}
 	}
-//	@Override
-//	public void onConfigurationChanged(Configuration newConfig) {
-//		super.onConfigurationChanged(newConfig);
-//		if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
-//			if (Constants.LOG_ENABLE) {
-//				Log.d("qiqi", "横屏");
-//			}
-//			setContentView(R.layout.mc_landscape);
-//		}else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-//			if (Constants.LOG_ENABLE) {
-//				Log.d("qiqi", "竖屏");
-//			}
-//			setContentView(R.layout.mc_portrait);
-//		}
-//		setListener();
-//	}
-//	@Override
-//	public void onWindowFocusChanged(boolean hasFocus) {
-//	        super.onWindowFocusChanged(hasFocus);
-//	    if (hasFocus) {
-//	    	mDecorView.setSystemUiVisibility(
-//	                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//	                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//	                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//	                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//	                | View.SYSTEM_UI_FLAG_FULLSCREEN
-//	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
-//	}
-	// This snippet hides the system bars.
-//	private void hideSystemUI() {
-//	    // Set the IMMERSIVE flag.
-//	    // Set the content to appear under the system bars so that the content
-//	    // doesn't resize when the system bars hide and show.
-//	    mDecorView.setSystemUiVisibility(
-//	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-//	            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-//	            | View.SYSTEM_UI_FLAG_IMMERSIVE);
-//	}
 
-	// This snippet shows the system bars. It does this by removing all the flags
+	// @Override
+	// public void onConfigurationChanged(Configuration newConfig) {
+	// super.onConfigurationChanged(newConfig);
+	// if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+	// if (Constants.LOG_ENABLE) {
+	// Log.d("qiqi", "横屏");
+	// }
+	// setContentView(R.layout.mc_landscape);
+	// }else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+	// if (Constants.LOG_ENABLE) {
+	// Log.d("qiqi", "竖屏");
+	// }
+	// setContentView(R.layout.mc_portrait);
+	// }
+	// setListener();
+	// }
+	// @Override
+	// public void onWindowFocusChanged(boolean hasFocus) {
+	// super.onWindowFocusChanged(hasFocus);
+	// if (hasFocus) {
+	// mDecorView.setSystemUiVisibility(
+	// View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	// | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	// | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	// | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	// | View.SYSTEM_UI_FLAG_FULLSCREEN
+	// | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+	// }
+	// This snippet hides the system bars.
+	// private void hideSystemUI() {
+	// // Set the IMMERSIVE flag.
+	// // Set the content to appear under the system bars so that the content
+	// // doesn't resize when the system bars hide and show.
+	// mDecorView.setSystemUiVisibility(
+	// View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	// | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	// | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	// | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+	// | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+	// | View.SYSTEM_UI_FLAG_IMMERSIVE);
+	// }
+
+	// This snippet shows the system bars. It does this by removing all the
+	// flags
 	// except for the ones that make the content appear under the system bars.
 	private void showSystemUI() {
-	    mDecorView.setSystemUiVisibility(
-	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+		mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 	}
+
 	Runnable getRun = new Runnable() {
 		@Override
 		public void run() {
@@ -171,7 +184,8 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 			/** NameValuePair是传送给服务器的请求参数 param.get("name") **/
 			/** 新建一个get请求 **/
 			HttpGet getTime = new HttpGet("http://vrfessor.com:12341/?action=1");
-			HttpGet getCategory = new HttpGet("http://vrfessor.com:12341/?action=2");
+			HttpGet getCategory = new HttpGet(
+					"http://vrfessor.com:12341/?action=2");
 			HttpGet getPics = new HttpGet("http://vrfessor.com:12341/?action=3");
 			HttpResponse response = null;
 			String strResult = "";
@@ -203,18 +217,19 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 				String category = "";
 				String mode = "";
 				String url = "";
-				
+
 				try {
-					//date
+					// date
 					String data3 = jsonObject1.getString("time");
 					JSONArray timeArray = new JSONArray(data3);
-					SharedPreferences sharedData = getSharedPreferences("qiqi", Context.MODE_PRIVATE);
+					SharedPreferences sharedData = getSharedPreferences("qiqi",
+							Context.MODE_PRIVATE);
 					String oDate = sharedData.getString("get_time", "");
-//					Log.d("qiqi", "timeArray:" + timeArray );
+					// Log.d("qiqi", "timeArray:" + timeArray );
 					String nDate = ((JSONArray) timeArray.get(0)).getString(1);
-//					Log.d("qiqi", " nDate:" + nDate);
-					if(nDate.equals(oDate)){
-						mHandler.sendEmptyMessage(0); 
+					// Log.d("qiqi", " nDate:" + nDate);
+					if (nDate.equals(oDate)) {
+						mHandler.sendEmptyMessage(0);
 						if (Constants.LOG_ENABLE) {
 							Log.d("qiqi", "data is same ,return.");
 						}
@@ -224,7 +239,8 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 						Log.d("qiqi", "data not same, download.");
 					}
 					getContentResolver().delete(Pic.CONTENT_URI, null, null);
-					getContentResolver().delete(Category.CONTENT_URI, null, null);
+					getContentResolver().delete(Category.CONTENT_URI, null,
+							null);
 					Editor editor = sharedData.edit();
 					editor.putString("get_time", nDate);
 					editor.commit();
@@ -233,10 +249,10 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 					e.printStackTrace();
 				}
 
-			} else{
+			} else {
 				Log.d("qiqi", "get提交失败");
 			}
-			//GET CATEGORY
+			// GET CATEGORY
 			try {
 				response = client.execute(getCategory);
 			} catch (ClientProtocolException e) {
@@ -261,11 +277,11 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 					e1.printStackTrace();
 				}
 				try {
-					//category
+					// category
 					String data3 = jsonObject1.getString("category");
 					JSONArray categoryArray = new JSONArray(data3);
-//					Log.d("qiqi", "categoryArray:" + categoryArray );
-					for(int i = 0; i < categoryArray.length();i++){
+					// Log.d("qiqi", "categoryArray:" + categoryArray );
+					for (int i = 0; i < categoryArray.length(); i++) {
 						ContentValues value = new ContentValues();
 						value.put(Category.COLUMN_DEFAULT_NAME,
 								((JSONArray) categoryArray.get(i)).getString(1));
@@ -273,16 +289,20 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 								((JSONArray) categoryArray.get(i)).getString(2));
 						value.put(Category.COLUMN_DEFAULT_LAND,
 								((JSONArray) categoryArray.get(i)).getString(3));
-//						Log.d("qiqi", "values:" + ((JSONArray) categoryArray.get(0)).getString(1) + " " + ((JSONArray) categoryArray.get(0)).getString(2) + " " + ((JSONArray) categoryArray.get(0)).getString(3));
-						getContentResolver().insert(Category.CONTENT_URI, value);
-					}	
-				}catch (JSONException e) {
-				// TODO Auto-generated catch block
+						// Log.d("qiqi", "values:" + ((JSONArray)
+						// categoryArray.get(0)).getString(1) + " " +
+						// ((JSONArray) categoryArray.get(0)).getString(2) + " "
+						// + ((JSONArray) categoryArray.get(0)).getString(3));
+						getContentResolver()
+								.insert(Category.CONTENT_URI, value);
+					}
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
-			//GET CATEGORY
+
+			// GET CATEGORY
 			try {
 				response = client.execute(getPics);
 			} catch (ClientProtocolException e) {
@@ -307,12 +327,12 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 					e1.printStackTrace();
 				}
 				try {
-					//category
+					// category
 					String data3 = jsonObject1.getString("pics");
 					JSONArray picArray = new JSONArray(data3);
-					Log.d("qiqi", "picArray.length:" + picArray.length() );
+					Log.d("qiqi", "picArray.length:" + picArray.length());
 					ArrayList<ContentValues> totalValues = new ArrayList<ContentValues>();
-					for(int i = 0; i < picArray.length();i++){
+					for (int i = 0; i < picArray.length(); i++) {
 						ContentValues value = new ContentValues();
 						value.put(Pic.COLUMN_DEFAULT_NAME,
 								((JSONArray) picArray.get(i)).getString(1));
@@ -322,25 +342,30 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 								((JSONArray) picArray.get(i)).getString(3));
 						value.put(Pic.COLUMN_DEFAULT_TAG,
 								((JSONArray) picArray.get(i)).getString(4));
-//						Log.d("qiqi", "values:" + ((JSONArray) picArray.get(i)).getString(1) + " " + ((JSONArray) picArray.get(i)).getString(2) + " " + ((JSONArray) picArray.get(i)).getString(3)+ " " + ((JSONArray) picArray.get(i)).getString(4));
+						// Log.d("qiqi", "values:" + ((JSONArray)
+						// picArray.get(i)).getString(1) + " " + ((JSONArray)
+						// picArray.get(i)).getString(2) + " " + ((JSONArray)
+						// picArray.get(i)).getString(3)+ " " + ((JSONArray)
+						// picArray.get(i)).getString(4));
 						getContentResolver().insert(Pic.CONTENT_URI, value);
-//						totalValues.add(value);
-//						insert(totalValues);
-					}	
+						// totalValues.add(value);
+						// insert(totalValues);
+					}
 					mHandler.sendEmptyMessage(0);
-				}catch (JSONException e) {
-				// TODO Auto-generated catch block
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
+
 		}
 	};
-	
+
 	private void startGetInfo() {
 
 		new Thread(getRun).start();
 	}
+
 	public JSONObject getJSON(String sb) throws JSONException {
 		return new JSONObject(sb);
 	}
@@ -357,16 +382,14 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 			for (int i = 0; i < values.size(); i++) {
 				rowId = db.insert(Pic.TABLE_NAME, null, values.get(i));
 				if (rowId != -1) {
-					rowUri = ContentUris
-							.withAppendedId(Pic.CONTENT_URI, rowId);
+					rowUri = ContentUris.withAppendedId(Pic.CONTENT_URI, rowId);
 				}
 			}
 			// 设置事务标志为成功，当结束事务时就会提交事务
 			db.setTransactionSuccessful();
 		} catch (Exception e) {
 
-		}
-		finally {
+		} finally {
 			// 结束事务
 			db.endTransaction();
 		}
@@ -374,4 +397,26 @@ public class ModeChoiceActivity extends Activity implements OnClickListener{
 		db.close();
 		return rowUri;
 	}
+
+	private boolean isConnected(){
+	ConnectivityManager mConnectivity = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);  
+	 TelephonyManager mTelephony = (TelephonyManager)this.getSystemService(TELEPHONY_SERVICE);  
+	 // 检查网络连接，如果无网络可用，就不需要进行连网操作等    
+	 NetworkInfo info = mConnectivity.getActiveNetworkInfo();    
+	 if (info == null ||   
+	         !mConnectivity.getBackgroundDataSetting()) {   
+	         return false;    
+	 }   
+	 //判断网络连接类型，只有在3G或wifi里进行一些数据更新。    
+	 int netType = info.getType();    
+	 int netSubtype = info.getSubtype();    
+	 if (netType == ConnectivityManager.TYPE_WIFI) {   
+	     return info.isConnected();    
+	 } else if (netType == ConnectivityManager.TYPE_MOBILE    
+	         && netSubtype == TelephonyManager.NETWORK_TYPE_UMTS    
+	         && !mTelephony.isNetworkRoaming()) {   
+	     return info.isConnected();    
+	 } else {   
+	     return false;    
+	 } }
 }
