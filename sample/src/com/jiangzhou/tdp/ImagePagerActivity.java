e23f548923336072469f9f63082f28c41312aab1
jiangzhouq <jiangzhouq@gmail.com>
2014-10-15 11:55:01 +0800

    fix BUG #411 横屏全屏查看单个图片，同时显示自动切换图标与虚拟按键时，自动切换图标被遮挡

--- sample/src/com/jiangzhou/tdp/ImagePagerActivity.java
+++ sample/src/com/jiangzhou/tdp/ImagePagerActivity.java
@@ -18,6 +18,7 @@ package com.jiangzhou.tdp;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.app.ActionBar;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.res.Configuration;
@@ -35,6 +36,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -91,7 +94,7 @@ public class ImagePagerActivity extends BaseActivity implements OnClickListener 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+//		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
 			getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		}else{
@@ -104,7 +107,13 @@ public class ImagePagerActivity extends BaseActivity implements OnClickListener 
 		mImageUrls= bundle.getStringArray(Extra.IMAGES);
 		mPagerPosition = bundle.getInt(Extra.IMAGE_POSITION, 0);
 
-
+		getActionBar().setDisplayShowTitleEnabled(false);
+		getActionBar().setDisplayShowHomeEnabled(false);
+		getActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.action_bar_bg));
+		getActionBar().hide();
+		
+		mDecorView = getWindow().getDecorView();
+		hideSystemUI();
 		options = new DisplayImageOptions.Builder()
 			.showImageForEmptyUri(R.drawable.ic_empty)
 			.showImageOnFail(R.drawable.ic_error)
@@ -125,11 +134,12 @@ public class ImagePagerActivity extends BaseActivity implements OnClickListener 
 			public void onTouchUp() {
 				mTimer = new Timer();
 				bar_show = !bar_show;
-				LinearLayout linear = (LinearLayout) findViewById(R.id.bar);
 				if(bar_show){
-					linear.setVisibility(View.VISIBLE);
+					showSystemUI();
+					getActionBar().show();
 				}else{
-					linear.setVisibility(View.INVISIBLE);
+					hideSystemUI();
+					getActionBar().hide();
 				}
 			}
 
@@ -154,19 +164,39 @@ public class ImagePagerActivity extends BaseActivity implements OnClickListener 
 			public void onPageScrollStateChanged(int arg0) {
 			}
 		});
-		ImageButton display = (ImageButton) findViewById(R.id.display);
-		display.setOnClickListener(this);
+//		ImageButton display = (ImageButton) findViewById(R.id.display);
+//		display.setOnClickListener(this);
 //		blue = (ImageButton) findViewById(R.id.bluetooth);
 //		blue.setOnClickListener(this);
-		
-		mDecorView = getWindow().getDecorView();
-		hideSystemUI();
 		
 		SensorManager sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		int sensorType = Sensor.TYPE_ACCELEROMETER;
 		sm.registerListener(myAccelerometerListener,sm.getDefaultSensor(sensorType),SensorManager.SENSOR_DELAY_NORMAL);
 		
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.pic_menu, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.huangdengpian:
+				mTimer = new Timer();
+				bar_show = !bar_show;
+				if(bar_show){
+					showSystemUI();
+				}else{
+					hideSystemUI();
+				}
+				mTimer.schedule(new MyTask(), 5000,5000);
+				return true;
+			default:
+				return false;
+		}
 	}
 	@Override
 	protected void onResume() {
@@ -234,17 +264,16 @@ public class ImagePagerActivity extends BaseActivity implements OnClickListener 
 	@Override
 	public void onClick(View view) {
 		switch(view.getId()){
-		case R.id.display:
-			mTimer = new Timer();
-			bar_show = !bar_show;
-			LinearLayout linear = (LinearLayout) findViewById(R.id.bar);
-			if(bar_show){
-				linear.setVisibility(View.VISIBLE);
-			}else{
-				linear.setVisibility(View.INVISIBLE);
-			}
-			mTimer.schedule(new MyTask(), 5000,5000);
-			break;
+//		case R.id.display:
+//			mTimer = new Timer();
+//			bar_show = !bar_show;
+//			if(bar_show){
+//				showSystemUI();
+//			}else{
+//				hideSystemUI();
+//			}
+//			mTimer.schedule(new MyTask(), 5000,5000);
+//			break;
 //		case R.id.bluetooth:
 //			switch(blue_state){
 //			case STATE_BLUE_DISCONNECTED:
@@ -436,20 +465,21 @@ public class ImagePagerActivity extends BaseActivity implements OnClickListener 
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
 	        super.onWindowFocusChanged(hasFocus);
-	    if (hasFocus) {
-	    	mDecorView.setSystemUiVisibility(
-	                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-	                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-	                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-	                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-	                | View.SYSTEM_UI_FLAG_FULLSCREEN
-	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+//	    if (hasFocus) {
+//	    	mDecorView.setSystemUiVisibility(
+//	                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//	                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//	                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//	                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//	                | View.SYSTEM_UI_FLAG_FULLSCREEN
+//	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
 	}
 	// This snippet hides the system bars.
 	private void hideSystemUI() {
 	    // Set the IMMERSIVE flag.
 	    // Set the content to appear under the system bars so that the content
 	    // doesn't resize when the system bars hide and show.
+		Log.d("qiqi", "hideSystemUI");
 	    mDecorView.setSystemUiVisibility(
 	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -462,6 +492,7 @@ public class ImagePagerActivity extends BaseActivity implements OnClickListener 
 	// This snippet shows the system bars. It does this by removing all the flags
 	// except for the ones that make the content appear under the system bars.
 	private void showSystemUI() {
+		Log.d("qiqi", "showSystemUI");
 	    mDecorView.setSystemUiVisibility(
 	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
